c780a57b29cb12704f510ae3a411590e0f4e6332
Sofialav <cemeteryvixen@gmail.com>
2020-03-17 14:29:32 +0100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import "./App.css";
 import EventsListContainer from "./components/EventsListContainer";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div>
+          <Navbar />
           <Route path="/" exact component={EventsListContainer} />
         </div>
       </Provider>
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  loginCheck = () => {
+    if (!this.props.user) {
+      const login = (
+        <Link to={"/login"}>
+          <div>Login</div>
+        </Link>
+      );
+      return login;
+    } else {
+      const login = (
+        <Link to={"/usertickets"}>
+          <div>My tickets</div>
+        </Link>
+      );
+      return login;
+    }
+  };
+  render() {
+    return (
+      <nav>
+        <Link to={"/"}>
+          <div>Home</div>
+        </Link>
+        {this.loginCheck()}
+      </nav>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+export default connect(mapStateToProps)(Navbar);
