a32f8962433b7e601161eaea016df95c3c0386fa
Sofialav <cemeteryvixen@gmail.com>
2020-03-20 12:43:19 +0100

    events pagination done

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -3,6 +3,34 @@ import { Link } from "react-router-dom";
 import moment from "moment";
 
 class EventsList extends Component {
+  nextButton = () => {
+    const nextPage = Number(this.props.pageId) + 1;
+    const offset = Number(this.props.pageId) * 9;
+    if (this.props.events.total > offset) {
+      return (
+        <div>
+          <Link to={`/eventsByPage/${nextPage}`}>
+            <button>Next page</button>
+          </Link>
+        </div>
+      );
+    }
+    return null;
+  };
+  prevButton = () => {
+    const prevPage = Number(this.props.pageId) - 1;
+    if (Number(this.props.pageId) > 1) {
+      return (
+        <div>
+          <Link to={`/eventsByPage/${prevPage}`}>
+            <button>Previous page</button>
+          </Link>
+        </div>
+      );
+    }
+    return null;
+  };
+
   render() {
     if (!Object.keys(this.props.events).length) {
       return <div>Loading...</div>;
@@ -22,14 +50,14 @@ class EventsList extends Component {
         </div>
       );
     });
-    const nextPage = Number(this.props.pageId) + 1;
     return (
       <div>
         <main>{displayEvents}</main>
-        <div>Total: {this.props.events.total}</div>
-        <Link to={`/eventsByPage/${nextPage}`}>
-          <button>Next page</button>
-        </Link>
+        <div>Total events: {this.props.events.total}</div>
+        <div>
+          <this.nextButton />
+          <this.prevButton />
+        </div>
       </div>
     );
   }
